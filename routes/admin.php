d2efb83848257c71d003847b4b7ddf51c95ade2c
Rinat Sarmuldin <ura07srr@gmail.com>
2024-05-23 21:09:17 +0500

    Setting up authorization in the Admin panel

--- routes/admin.php
+++ routes/admin.php
@@ -2,4 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::resource('posts', PostController::class);
+
+Route::get('login', [\App\Http\Controllers\Admin\AuthController::class, 'index'])->name('admin.login');
+Route::post('login_process', [\App\Http\Controllers\Admin\AuthController::class, 'login'])->name('admin.login_process');
+
+Route::middleware("auth:admin")->group(function () {
+    Route::resource('posts', PostController::class);
+});
+
+
+
